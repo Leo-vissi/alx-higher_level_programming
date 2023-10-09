3b2674e36372ccc521af6e097415a09738bd43d6
Leo-vissi <ahmlot92gmail.com>
2023-10-09 15:06:22 -0700

    tasks

--- 0x03-python-data_structures/linked_lists.c
+++ 0x03-python-data_structures/linked_lists.c
@@ -11,7 +11,7 @@
 size_t print_listint(const listint_t *h)
 {
     const listint_t *current;
-    unsigned int n; /* number of nodes */
+    unsigned int n;
 
     current = h;
     n = 0;
@@ -31,7 +31,6 @@ size_t print_listint(const listint_t *h)
  * @n: integer to be included in new node
  * Return: address of the new element or NULL if it fails
  */
-
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
     listint_t *new;
@@ -63,7 +62,6 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
  * @head: pointer to list to be freed
  * Return: void
  */
-
 void free_listint(listint_t *head)
 {
     listint_t *current;
